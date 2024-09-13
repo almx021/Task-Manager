9b281348fbb8b682e5981b22184804709bc56b31
André Lucas <andre.lucas.mendes01@aluno.ifce.edu.br>
2024-09-13 16:36:22 -0300

    feature: Add Task Model

--- src/main/java/com/mendes/task_manager/TaskManagerApplication.java
+++ src/main/java/com/mendes/task_manager/TaskManagerApplication.java
@@ -2,8 +2,10 @@ package com.mendes.task_manager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class TaskManagerApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/mendes/task_manager/model/Task.java
@@ -0,0 +1,94 @@
+package com.mendes.task_manager.model;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="tb_tasks")
+@EntityListeners(AuditingEntityListener.class)
+public class Task {
+    public Task() {}
+
+    public Task(Long id, String title, String description, String createdAt, String lastUpdatedAt) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.createdAt = createdAt;
+        this.lastUpdatedAt = lastUpdatedAt;
+    }
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name="task_id")
+    Long id;
+
+    @Column(name="task_title", length=50, nullable=false)
+    String title;
+
+    @Column(name="task_description", length=255, nullable=false)
+    String description;
+
+    @CreatedDate
+    @Column(name="created_at", updatable=false, nullable=false)
+    String createdAt;
+
+    @LastModifiedDate
+    @Column(name="updated_at", nullable=false)
+    String lastUpdatedAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getLastUpdatedAt() {
+        return lastUpdatedAt;
+    }
+
+    public void setLastUpdatedAt(String lastUpdatedAt) {
+        this.lastUpdatedAt = lastUpdatedAt;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Task %s: %s\n%s", id, title, description);
+    }
+
+    
+}
